d54ccb9b370064e9995deeb363a5df8af7a5fbac
ayrigom <85459471+ayrigom@users.noreply.github.com>
2025-06-28 11:34:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from mcstatus import JavaServer
 ENDERECO_SERVIDOR = "147.185.221.27"  # Coloque o IP ou domínio aqui
 PORTA = 25901  # Porta do servidor Minecraft
 INTERVALO = 10  # Tempo entre verificações (em segundos)
-WEBHOOK_URL = "https://l.webhook.party/hook/pVV5SDBqnixgp7auP4VWiMA46grZwbhJWv7C5ZyWe6B4qVqC8nLTiDB5me6d6lR5eUUzM6L3iOnJbYGrNcWMMg3L60bjh1u2BWM3qkg49pHb41YQNgBvMTNzPPIB1ROUx8WZBKltzjZkttPVCmMYvNPpB6hEqLSRnafeLePQN0jClOqla35nmOqe4E3vpPIeakE6SKN2a5bWhNpShzYeCYCT0eTadZ%2F%2FHUc%2ByXp3K2rUsyFsejRRAsGA30jE0HWO82lkS5rLJj1Ga%2FisLRn7vK4R90JkiEKysNgSGQYgKaxdm7UMsfQ2lsxEF5wbh0yD7LCVsrfaRQkYmrOr0jWNZMSmA7NqOw4U7j1aiH%2FsfjDnbmb930gfFFgw1MI12HaDUnQgzEcV0H8%3D/nrAs0vJMKf%2Fcrn1n"  # Substitua pelo seu webhook
+WEBHOOK_URL = "https://discord.com/api/webhooks/1387204459989307392/xHlbN2clVZ7Doia-_gkIIxnYlq2aUrevdi6c669HcvIRNFy7gIF1ZDeGQlSqCzL-fInE"  # Substitua pelo seu webhook
 
 # === VARIÁVEL DE CONTROLE ===
 ultimo_status_online = None  # Começa sem saber o status
